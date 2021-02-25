a800c8d16349ff078ef352d467c2d33a5f17fb5c
Admin <jamvin75@gmail.com>
2021-02-25 20:40:43 +0200

    first

---
+++ 2 task.py
@@ -0,0 +1,28 @@
+#2 завдання
+b = int (input())
+if (b == 1):
+    print("Січень")
+elif (b == 2):
+    print("Лютий")
+elif (b == 3):
+    print("Березень")
+elif (b == 4):
+    print("Квітень")
+elif (b == 5):
+    print("Травень")
+elif (b == 6):
+    print("Червень")
+elif (b == 7):
+    print("Липень")
+elif (b == 8):
+    print("Серпень")
+elif (b == 9):
+    print("Вересень")
+elif (b == 10):
+    print("Жовтень")
+elif (b == 11):
+    print("Листопад")
+elif (b == 12):
+    print("Грудень")
+else :
+    print("error")
